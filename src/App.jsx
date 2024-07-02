843be09a25c426f39641b50880225e32ac443ebf
LebogangMputle <lebozikalala@gmail.com>
2024-07-02 16:45:44 +0200

    "Updated index.html, App.css, App.jsx, and various component files (Courses, Insights, Navbar, Header) with new styles and layouts; removed LandingPage and Home components."

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,22 @@
-// import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./components/LandingPage/LandingPage";
-import Home from "./components/Home";
-import Courses from "./components/Courses/Courses";
-import AboutUs from "./components/About/AboutUs";
-import ContactUs from "./components/About/ContactUs";
-import OurServices from "./components/About/OurServices";
-
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Courses from './components/Courses/Courses';
+import AboutUs from './components/About/AboutUs';
+import Navbar from './components/Navbar/Navbar';
+import Insights from './components/Insights/Insights';
+import './App.css'
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<LandingPage />}>
-            <Route path="home" element={<Home />} />
-            <Route path="courses" element={<Courses />} />
-            <Route path="aboutUs" element={<AboutUs/>} />
-            <Route path="contactUs" element={<ContactUs/>} />
-            <Route path="services" element={<OurServices />} />
-          </Route>
-        </Routes>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/aboutUs" element={<AboutUs />} />
+        <Route path="/insights" element={<Insights />} />
+      </Routes>
     </Router>
-
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
-const Navbar
- = () => {
-    return (
-        <nav className="nav">
-          <Link href="/">Home</Link>
-          <Link href="/Services">Services</Link>
-          <Link href="/Courses">Courses</Link>
-          <Link href="/about">about</Link>
-          <Link href="/insights">Insights</Link>
-        </nav>
-    );
-  };
-  
-  export default Navbar;
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item"><Link to="/">Home</Link></li>
+        <li className="navbar-item"><Link to="/courses">Courses</Link></li>
+        <li className="navbar-item"><Link to="/insights">Insights</Link></li>
+        <li className="navbar-item"><Link to="/aboutUs">AboutUs</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
