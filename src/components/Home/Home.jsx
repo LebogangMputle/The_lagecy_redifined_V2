e85f7c414b8436ec6c82b782d1d4660a52f48105
LebogangMputle <lebozikalala@gmail.com>
2024-08-28 09:43:49 +0200

    "Updated Home component CSS and JSX: added flexbox styling, centered text, and revised paragraph content and structure."

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,8 +4,18 @@ const Home = () => {
   return (
     <div className="content">
     <h1 className="H1">Legacy Redefined</h1>
-    <p>Redefining business processes and risk</p>
-    <p>Your distinguished business process and risk experts</p>
+    <h2>Vision:</h2>
+    <p>We strive to assist in establishing sound business
+processes underpinned by rigorous risk management.
+By thoroughly integrating these elements, long term
+sustainability will be achieved.</p>
+    
+    <h2>Mission:</h2>
+    <p>We intend on providing our clients with expert guidance
+and support in developing robust business processes and
+comprehensive risk management strategies. Our
+dedicated approach ensures that our clients achieve
+operational excellence and long-term sustainability.</p>
   </div>
   );
 };
