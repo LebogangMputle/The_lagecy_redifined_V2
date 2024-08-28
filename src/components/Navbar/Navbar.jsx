bcd529b018e1e2c60bed1ee3a1fccbb2ca48774e
LebogangMputle <lebozikalala@gmail.com>
2024-08-28 09:32:21 +0200

    "Updated Navbar component to import logo image and display it instead of text"

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.jpeg'; // Corrected import path
 import './Navbar.css';
 
 const Navbar = () => {
@@ -13,7 +14,11 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">
-          Logo
+          <img 
+            src={logo} 
+            alt="Logo" 
+            className="logo-image" 
+          />
         </Link>
         <div className="menu-icon" onClick={handleMenuToggle}>
           &#9776; {/* Hamburger menu icon */}
