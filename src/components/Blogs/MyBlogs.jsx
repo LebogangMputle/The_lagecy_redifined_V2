1218b6b3d0ab357f5709a84a5a8431a2a02a3574
LebogangMputle <lebozikalala@gmail.com>
2024-09-23 10:49:55 +0200

    "Deleted react.svg file, updated MyBlogs.css with new styles, and modified MyBlogs.jsx to add state for expanded blog, toggle read more functionality, and updated blog data with new images and descriptions."

--- src/components/Blogs/MyBlogs.jsx
+++ src/components/Blogs/MyBlogs.jsx
@@ -1,35 +1,54 @@
-import React from 'react';
+import { useState } from 'react';
+import Screenshot from '../../assets/Screenshot.png';
+import Screenshot2 from '../../assets/Screenshot2.png';
+import Screenshot3 from '../../assets/Screenshot3.png';
+// import blog4 from '../../assets/blog4.jpeg';
+// import blog5 from '../../assets/blog5.jpeg';
 import './MyBlogs.css';
 
 const MyBlogs = () => {
+  const [expandedBlog, setExpandedBlog] = useState(null);
+
   const blogs = [
     {
-      title: "Blog 1",
-      description: "Description for Blog 1",
-      image: "/assets/blog1.png", // Relative path from public folder
+      title: "Romanticize your leadership style",
+      shortDescription: "Short description for Blog 1",
+      fullDescription: "Full description for Blog 1. This is where the longer version of the blog description will go.",
+      image: Screenshot // Reference from public folder
     },
     {
       title: "Blog 2",
-      description: "Description for Blog 2",
-      image: "/assets/blog2.PNG",
+      shortDescription: "Short description for Blog 2",
+      fullDescription: "Full description for Blog 2. This is where the longer version of the blog description will go.",
+      image: Screenshot2,
     },
     {
       title: "Blog 3",
-      description: "Description for Blog 3",
-      image: "/assets/blog3.jpeg",
+      shortDescription: "Short description for Blog 3",
+      fullDescription: "Full description for Blog 3. This is where the longer version of the blog description will go.",
+      image: Screenshot3,
     },
-    {
-      title: "Blog 4",
-      description: "Description for Blog 4",
-      image: "/assets/blog4.jpeg",
-    },
-    {
-      title: "Blog 5",
-      description: "Description for Blog 5",
-      image: "/assets/blog5.jpeg",
-    },
+    // {
+    //   title: "Blog 4",
+    //   shortDescription: "Short description for Blog 4",
+    //   fullDescription: "Full description for Blog 4. This is where the longer version of the blog description will go.",
+    //   image: blog4,
+    // },
+    // {
+    //   title: "Blog 5",
+    //   shortDescription: "Short description for Blog 5",
+    //   fullDescription: "Full description for Blog 5. This is where the longer version of the blog description will go.",
+    //   image: blog5,
+    // },
   ];
-  
+
+  const toggleReadMore = (index) => {
+    if (expandedBlog === index) {
+      setExpandedBlog(null); // Collapse the description if it's already expanded
+    } else {
+      setExpandedBlog(index); // Expand the clicked blog
+    }
+  };
 
   return (
     <section className="blogs-section">
@@ -39,7 +58,12 @@ const MyBlogs = () => {
           <div key={index} className="blog-card">
             <img src={blog.image} alt={blog.title} className="blog-image" />
             <h3>{blog.title}</h3>
-            <p>{blog.description}</p>
+            <p>
+              {expandedBlog === index ? blog.fullDescription : blog.shortDescription}
+            </p>
+            <button className="read-more-button" onClick={() => toggleReadMore(index)}>
+              {expandedBlog === index ? 'Read Less' : 'Read More'}
+            </button>
           </div>
         ))}
       </div>
